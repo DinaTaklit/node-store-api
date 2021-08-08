3abf87a339cb796fbf017626d7ec403f08dba609
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 13:53:05 +0100

    fix: routes: products: fix a typo so the import work

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import {getAllProducts, getAllProductsStatic} from './../../controllers/products'
+import {getAllProducts, getAllProductsStatic} from './../controllers/products.js'
 
-const router = express.Router // to create routes
+const router = express.Router() // to create routes
 
 router.route('/').get(getAllProducts) // Route to get Products with dinamic queries
 router.route('/static').get(getAllProductsStatic) // Static route to get products
