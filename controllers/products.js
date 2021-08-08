37f2e26d7db6c4b0ff640c5952bd82ec84296731
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 15:17:59 +0100

    feat: controllers: products: Select option

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,14 @@ import Product from '../models/product.js'
 
 // Function that get all products statically
 export const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({}).sort('-name price').select('name price')
     res.status(200).json({nbHits: products.length, products})
 }
 
 // function that get all products dinamically
 
 export const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query // destrctured query parameters
+    const {featured, company, name, sort, fields} = req.query // destrctured query parameters
     const queryObject = {} // construct a new query object based on the query we get from the user
 
     // Check for the existance of query parameters
@@ -30,13 +30,26 @@ export const getAllProducts = async (req, res) => {
         queryObject.name = {$regex: name, $options: 'i'}
     }
 
+    // create the result var with find
     let result = Product.find(queryObject)
+
     // Add sort to the object if exist
     if (sort){
-        const sortList = sort.split(',').join(' ')
-        result = result.sort(sortList)
+        const sortList = sort.split(',').join(' ') // get sort list
+        result = result.sort(sortList) // append sort to the result
+    }  else {
+        // if no sort giving we sort by created date
+        result = result.sort('createAt')
     }
     
-    const products = await result
+
+    // Select according of the fields if given
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ') // get fields list
+        result = result.select(fieldsList) // append fields to the result
+    }
+
+    const products = await result // run result query
+
     res.status(200).json({nbHits: products.length, products})
 }   
