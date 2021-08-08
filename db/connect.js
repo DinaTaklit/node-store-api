3e4e74fe9d00fda5fbb01614dbc7f001d7426118
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 07:14:58 +0100

    feat: db: connect.js: impelemnt the function that is responsible to connct to the database

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+// Function to connect to the database
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
+
+export default connectDB
