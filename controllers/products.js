178bd1a01ab0be3cb80c2195cf3486845e12d364
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 14:33:05 +0100

    feat: controllers: products: company

--- controllers/products.js
+++ controllers/products.js
@@ -12,7 +12,7 @@ export const getAllProductsStatic = async (req, res) => {
 // function that get all products dinamically
 
 export const getAllProducts = async (req, res) => {
-    const {featured} = req.query // destrctured query parameters
+    const {featured, company} = req.query // destrctured query parameters
     const queryObject = {} // construct a new query object based on the query we get from the user
 
     // Check for the existance of query parameters
@@ -20,7 +20,11 @@ export const getAllProducts = async (req, res) => {
     if (featured){
         queryObject.featured = featured
     }
-    
+
+    if (company){
+        queryObject.company = company
+    }
+
     console.log(queryObject)
     const products = await Product.find(queryObject)
     res.status(200).json({nbHits: products.length, products})
