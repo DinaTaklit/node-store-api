02daccb065d8e5a804820b7d6ab2c79f3da5a7a7
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 14:56:45 +0100

    feat: controllers: products: Sort implementation

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ export const getAllProductsStatic = async (req, res) => {
 // function that get all products dinamically
 
 export const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query // destrctured query parameters
+    const {featured, company, name, sort} = req.query // destrctured query parameters
     const queryObject = {} // construct a new query object based on the query we get from the user
 
     // Check for the existance of query parameters
@@ -30,7 +30,13 @@ export const getAllProducts = async (req, res) => {
         queryObject.name = {$regex: name, $options: 'i'}
     }
 
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    // Add sort to the object if exist
+    if (sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    
+    const products = await result
     res.status(200).json({nbHits: products.length, products})
 }   
