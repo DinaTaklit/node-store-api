942f9272a0df76599874c95b267e13e8b7478836
DinaTaklit <ez_taklit@esi.dz>
2021-08-08 14:16:29 +0100

    feat: controllers: products: refactor to query object

--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,25 @@ import Product from '../models/product.js'
 
 // Function that get all products statically
 export const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
+    const products = await Product.find({
+        page:'2'
+    })
     res.status(200).json({nbHits: products.length, products})
 }
 
 // function that get all products dinamically
 
 export const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
+    const {featured} = req.query // destrctured query parameters
+    const queryObject = {} // construct a new query object based on the query we get from the user
+
+    // Check for the existance of query parameters
+
+    if (featured){
+        queryObject.featured = featured
+    }
+    
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({nbHits: products.length, products})
 }   
